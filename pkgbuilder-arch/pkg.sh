7d17f85178ae7c85ec658a71f1e779a8385429ea
ConorIA <conor.anderson@gmail.com>
2016-08-27 23:46:32 -0400

    improve pkgbuilder:arch based on pkgbuilder:manjaro

--- pkgbuilder-arch/pkg.sh
+++ pkgbuilder-arch/pkg.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == '--help' ] || [ "$1" == '-h' ]; then
   echo "Navigate to the folder containing your makefile and execute:"
-  echo "docker run --rm -v \"\$(pwd):/src\" conoia/pkgbuilder"
+  echo "docker run --rm -v \"\$(pwd):/src\" conoria/pkgbuilder:arch"
   exit 1
 fi
 
@@ -11,9 +11,14 @@ pacman -Sy --noprogressbar --noconfirm
 pacman -S --noprogressbar --noconfirm archlinux-keyring manjaro-keyring
 pacman -Su --noprogressbar --noconfirm
 
-echo "Installing dependencies and building package"
-sudo -u maker makepkg -scf --noconfirm
+echo "Installing packages and building package"
+deps=$(awk '/depends\=/{print}' PKGBUILD | sed 's/.*(//g' | sed 's/)//g' | sed "s/'//g")
+sudo -u maker pacaur -S --needed --noconfirm $deps
+sudo -u maker makepkg -cf --noconfirm
 
 echo "Checking package with namcap"
 result=$(find *.pkg.tar.xz)
 namcap $result
+
+echo "Testing if package can be installed"
+yes | pacman -U $result
