1e3902a4a61d9d23059573e6ec6acf9acc0634d6
ConorIA <conor.anderson@gmail.com>
2016-09-05 20:42:06 -0400

    auto-update fixes

--- pkgbuilder-arch/sh/auto-update.sh
+++ pkgbuilder-arch/sh/auto-update.sh
@@ -5,15 +5,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 pacman -Syyu --noconfirm
 ./mkimage-arch.sh
-docker run manjaro bash
+cd ..
+docker run archlinux bash
 str=$(docker ps -a | grep "archlinux")
-docker export "${str%% *}" | xz > ../archlinux.tar.xz
+docker export "${str%% *}" | xz > archlinux.tar.xz
 docker rm "${str%% *}"
 docker rmi archlinux
 docker build -t conoria/pkgbuilder:arch .
 docker rmi $(docker images -q --filter "dangling=true")
 docker push conoria/pkgbuilder:arch
-cd ../..
+cd ..
 git add pkgbuilder-manjaro/archlinux.tar.xz
 git commit -m "Auto-update pkgbuilder:arch"
 git push
--- pkgbuilder-manjaro/sh/auto-update.sh
+++ pkgbuilder-manjaro/sh/auto-update.sh
@@ -5,15 +5,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 pacman -Syyu --noconfirm
 ./mkimage-manjaro.sh
+cd ..
 docker run manjaro bash
 str=$(docker ps -a | grep "manjaro")
-docker export "${str%% *}" | xz > ../manjarolinux.tar.xz
+docker export "${str%% *}" | xz > manjarolinux.tar.xz
 docker rm "${str%% *}"
 docker rmi manjaro
 docker build -t conoria/pkgbuilder:manjaro .
 docker rmi $(docker images -q --filter "dangling=true")
 docker push conoria/pkgbuilder:manjaro
-cd ../..
+cd ..
 git add pkgbuilder-manjaro/manjarolinux.tar.xz
 git commit -m "Auto-update pkgbuilder:manjaro"
 git push
