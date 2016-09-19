eaf60e8fa72b1a572c83681d5920be8382ecf7fb
Conor Anderson <conor.anderson@gmail.com>
2016-09-19 18:49:45 -0400

    auto update fixes
    Former-commit-id: 13b7ace2ad0b79ce9a6db154353852600a11eafa

--- pkgbuilder-arch/sh/auto-update.sh
+++ pkgbuilder-arch/sh/auto-update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-pacman -Syyu --noconfirm
-./mkimage-arch.sh
+
+set -e
+
+sudo pacman -Syyu --noconfirm
+sudo ./mkimage-arch.sh
+
 cd ..
 docker run archlinux bash
 str=$(docker ps -a | grep "archlinux")
@@ -12,13 +12,15 @@ docker export "${str%% *}" | xz > archlinux.tar.xz
 docker rm "${str%% *}"
 docker rmi archlinux
 docker build -t conoria/pkgbuilder:arch .
-docker rmi $(docker images -q --filter "dangling=true")
+docker rmi $(docker images -q --filter "dangling=true") 2>&1 || echo $?
 docker push conoria/pkgbuilder:arch
 sed -i '$d' README.md
 date +%D >> README.md
+
 cd ..
 git add pkgbuilder-arch/README.md
 git add pkgbuilder-arch/archlinux.tar.xz
 git commit -m "Auto-update pkgbuilder:arch"
 git push
+
 echo "Full auto-update process complete"
--- pkgbuilder-manjaro/sh/auto-update.sh
+++ pkgbuilder-manjaro/sh/auto-update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-pacman -Syyu --noconfirm
-./mkimage-manjaro.sh
+
+set -e
+
+sudo pacman -Syyu --noconfirm
+sudo ./mkimage-manjaro.sh
+
 cd ..
 docker run manjaro bash
 str=$(docker ps -a | grep "manjaro")
@@ -12,13 +12,15 @@ docker export "${str%% *}" | xz > manjarolinux.tar.xz
 docker rm "${str%% *}"
 docker rmi manjaro
 docker build -t conoria/pkgbuilder:manjaro .
-docker rmi $(docker images -q --filter "dangling=true")
+docker rmi $(docker images -q --filter "dangling=true") 2>&1 || echo $?
 docker push conoria/pkgbuilder:manjaro
 sed -i '$d' README.md
 date +%D >> README.md
+
 cd ..
 git add pkgbuilder-manjaro/README.md
 git add pkgbuilder-manjaro/manjarolinux.tar.xz
 git commit -m "Auto-update pkgbuilder:manjaro"
 git push
+
 echo "Full auto-update process complete"
